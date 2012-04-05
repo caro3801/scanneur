a2ef151f4acdd7bbf1882d21e053696938a5b2c8
caroline.desprat <caroline.desprat@4d4d53e2-05cd-420c-8e85-1ab36e17ddb0>
2012-04-05 11:43:57 +0000

    exception

---
+++ src/scanneur/ScanneurException.java
@@ -0,0 +1,15 @@
+package scanneur;
+
+/**
+ * Messages d'exception pour une application d'exception :)
+ */
+
+public class ScanneurException extends Exception{
+	public ScanneurException(){
+		super();
+	}
+
+	public ScanneurException(String message){
+		super(message);
+	}
+}
