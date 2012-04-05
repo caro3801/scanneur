d6c7085c26144c8543d2c18bfce89ab86646bf18
caroline.desprat <caroline.desprat@4d4d53e2-05cd-420c-8e85-1ab36e17ddb0>
2012-04-05 13:25:41 +0000

    aaa

--- trunk/src/scanneur/Scanneur.java
+++ trunk/src/scanneur/Scanneur.java
@@ -19,12 +19,6 @@ public class Scanneur extends Thread {
     /**
      * @param args the command line arguments
      */
-    JLabel jlabel;
-    InetAddress adresse;
-    int portDebut;
-    int portFin;
-    int nbThread;
-    boolean configOK = false;
     int lowestPort;
     int highestPort;
     InetAddress host;
@@ -33,36 +27,7 @@ public class Scanneur extends Thread {
     boolean tcp = true;
     public static int NBTHREAD = 10;
 
-    Scanneur(String adresse, boolean udp, boolean tcp, String portD, String portF, boolean plage, String nbThread, JLabel jlabel) {
-        this.jlabel = jlabel;
-        jlabel.setText("");
-        try {
-            this.adresse = InetAddress.getByName(adresse);
-            if (!udp && !tcp) {
-                jlabel.setText("ERREUR : veuillez choisir au moins un mode de connection");
-            } else {
-                this.udp = udp;
-                this.tcp = tcp;
-                portDebut = Integer.parseInt(portD);
-                portFin = Integer.parseInt(portF);
-                if ((portDebut > portFin && plage) || portDebut < 0 || portFin < 0 || portDebut > 65000 || portFin > 65000) {
-                    jlabel.setText("ERREUR : veuillez configurer correctement les ports");
-                } else {
-                    if (!plage) {
-                        this.portFin = this.portDebut;
-                    }
-                    this.nbThread = Integer.parseInt(nbThread);
-                    if (this.nbThread < 0) {
-                        jlabel.setText("ERREUR : nombre de thread négatif");
-                    } else {
-                        configOK = true;
-                    }
-                }
-            }
-        } catch (UnknownHostException ex) {
-            jlabel.setText(adresse + " inconnue");
-        }
-    }
+   
 
     public Scanneur(String hostname) throws ScanneurException {
         try {
